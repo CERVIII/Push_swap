53ef60bbe660212d9f73018379a506df0d568df1
Pablo Cervilla <pablo.cervilla117@gmail.com>
2024-04-01 18:39:13 +0200

    norminette

--- src/algoritm_100_500.c
+++ src/algoritm_100_500.c
@@ -6,7 +6,7 @@
 /*   By: pcervill <pcervill@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 12:10:12 by pcervill          #+#    #+#             */
-/*   Updated: 2024/04/01 13:16:54 by pcervill         ###   ########.fr       */
+/*   Updated: 2024/04/01 18:29:25 by pcervill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,9 @@ void	chunkslen(t_stacks *data)
 		3º) Creo una variable para el ultimo numero de la pila.
 		4º) Empujo el numero a la pila b.
 		5º) Compruebo el tamaño de la pila.
-		6º) Si el tamaño es mayor o igual a 500, el bloque es de 25.
-		7º) Si el tamaño es mayor o igual a 100 y menor a 500, el bloque es de 10.
-		8º) Si el tamaño es mayor a 5, el bloque es de 3.
+				Si el tamaño es >= a 500, el bloque es de 25.
+				Si el tamaño es >= a 100 y < a 500, el bloque es 10.
+				Si el tamaño es > a 5, y < 100 el bloque es de 3.
 		9º) Si la pila b tiene mas de 2 numeros y el contenido del primer nodo
 			es menor que el numero del bloque y la pila a tiene mas de 2 numeros
 			y el contenido del primer nodo es mayor que el ultimo numero, hago
@@ -60,7 +60,7 @@ void	push_min(t_list **stack_a, t_list **stack_b, t_stacks *data, int i)
 	if (data->count_b >= 2 && (*stack_b)->content < data->cpy[i - block]
 		&& data->count_a >= 2 && (*stack_a)->content > data->lastnum)
 		ft_rr(stack_a, stack_b, data);
-	if (data->count_b >= 2 && (*stack_b)->content < data->cpy[i - block])
+	else if (data->count_b >= 2 && (*stack_b)->content < data->cpy[i - block])
 		ft_rb(stack_b, data);
 }
 
@@ -83,10 +83,7 @@ void	rotate_max(t_list **stack_a, t_list **stack_b, t_stacks *data, int i)
 		ft_ra(stack_a, data);
 }
 
-/*
-	Funcion para empujar los numeros de la pila A a la pila B, segun los
-	bloques establecidos
-*/
+
 void	push_to_b(t_list **stack_a, t_list **stack_b, t_stacks *data)
 {
 	int	i;
